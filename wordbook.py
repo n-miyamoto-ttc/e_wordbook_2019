c99ad9d1e3ac26641b50bfeb441193b16e412909
w-akita <ttcwakita@gmail.com>
2019-04-17 11:23:19 +0900

    コードを更新

--- wordbook.py
+++ wordbook.py
@@ -3,8 +3,24 @@
 
 def main():
     print("私は英単語帳アプリです。")
-    print("私は秋田若奈です")
-
+    print("私は秋田若奈です\n")
 
 if __name__ == '__main__':
     main()
+
+
+
+
+with open("kari.txt","r",encoding = "utf-8") as f:
+    kari_text = f.read()
+
+import re
+
+
+kari = []
+moji = re.findall("\D\w+",kari_text)
+for s in moji:
+    kari.append(s.lower())
+
+
+print(kari)
