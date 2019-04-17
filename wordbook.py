b8de4be343f6e8fbafce94aecbbf01f508be46ee
genri <g-ri@ttc-net.co.jp>
2019-04-17 11:24:09 +0900

    Revert "言い方変更"
    This reverts commit d4bbc339ccbd1e3618ec4807a8b5a29df8ce70c7.

--- wordbook.py
+++ wordbook.py
@@ -1,17 +1,10 @@
 # coding= utf-8
-import re
-from collections import Counter
+
+
 def main():
     print("私は英単語帳アプリです。")
-    f = open("input\\article1.txt","r",encoding="utf-8")
-    fr = f.read()
-    words = re.split("\\s|\,|\.|\(|\)|\-|[0-9]", fr.lower())
-    print(words)
-    counter = Counter(words)
-    print(counter)
-    for word, count in counter.most_common():
-        if len(word) > 0:
-            print("%s,%d" % (word, count))
+    print("Start")
+
 
 if __name__ == '__main__':
     main()
