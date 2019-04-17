f5838d47166c531d281aa4b2ff1850c3c0fdad69
watanabe9101 <y-watanabe@ttc-net.co.jp>
2019-04-17 11:25:06 +0900

    2019-4-17

--- wordbook.py
+++ wordbook.py
@@ -1,10 +1,9 @@
 # coding= utf-8
+    
+# def main():
+#     print("私は英単語帳アプリです。")
+#     print("私は渡辺　雄です。")
 
 
-def main():
-    print("私は英単語帳アプリです。")
-    print("私は渡辺　雄です。")
-
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
