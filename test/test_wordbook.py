f5e018b82ad8350984fe300e9ca9fa25f8771a1a
watanabe9101 <y-watanabe@ttc-net.co.jp>
2019-04-19 11:39:11 +0900

    コーディング終了テスト前まで

--- test/test_wordbook.py
+++ test/test_wordbook.py
@@ -11,11 +11,16 @@ class WordBookTest(unittest.TestCase):
     """Test to wordbook
     """
 
-    def test_print_txt(self):
-        expected = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.\n"
-        actual = wordbook.print_txt("sample.txt")
+    def test_remove(self):
+        expected = ["hogehoge"]
+        actual = wordbook.remove(["as", "the", "hogehoge"], "input\\remove.txt")
         self.assertEqual(expected, actual)
 
+    def test_sort_format(self):
+        expected  = [["g", 1000], ["k", 110], ["e", 100], ["i", 100], ["l", 99], ["h", 89], ["j", 53], ["d", 30], ["b", 15], ["a", 10]]
+        test_case = {"a": 10, "b": 15, "c": 1, "d": 30, "e": 100, "f": 0, "g": 1000, "h": 89, "i": 100, "j": 53, "k": 110, "l": 99}
+        actual = wordbook.sort_format(test_case)
+        self.assertEqual(expected, actual)
 
 if __name__ == "__main__":
     unittest.main()
--- wordbook.py
+++ wordbook.py
@@ -5,54 +5,60 @@ import json
 def main():
     menu()
 
-# メニューを表示してそれぞれの処理を行う
+# メニューを表示して入力された番号でそれぞれの処理を行う
 def menu():
+    print("\nメニュー選択")
     menu_list = [
         "英単語帳作成　　: 1", 
-        "日本語訳の更新　: 2",
+        "日本語訳更新　　: 2",
         "英単語帳一覧　　: 3",
         "ソート方法選択　: 4",
         "終了　　　　　　: 0"
     ]
     for s in menu_list:
         print(s)
-    print("メニューから行うことを数字で選んでください")
-    try:
-        select = int(input())
-        if select == 1:
-            print("英単語帳作成")
-            create_wordbook()
-        elif select == 2:
-            print("日本語訳の更新")
-            update_wordbook()
-        elif select == 3:
-            print("英単語帳一覧")
-            print_word_count_mean()
-        elif select == 4:
-            print("ソート方法選択")
-        elif select == 0:
-            print("終了します")
-        else:
-            print("その数字は存在しません")
-            menu()
-    except ValueError:
-        print("数字を入力してください")
+    print("メニューから行いたいことの番号を半角で入力してください")
+    select = input()
+
+    if select == "1":
+        print("英単語帳作成")
+        create_wordbook()
+        menu()
+    elif select == "2":
+        print("日本語訳の更新")
+        update_wordbook()
+        menu()
+    elif select == "3":
+        print("英単語帳一覧")
+        print_word_count_mean()
+        menu()
+    elif select == "4":
+        print("ソート方法選択")
+        sort()
+        menu()
+    elif select == "0":
+        print("終了します")
+    else:
+        print("メニューに存在する番号を半角で入力してください")
         menu()
         
-# 元の文から英単語帳を作成する
+# 元の英文テキストファイルから英単語帳を作成する
 def create_wordbook():
-    s_pass = "input\\article1.txt"
+    s_pass   = "input\\article1.txt"
     r_pass   = "input\\remove.txt"
-    word_dict = count_per_word(cut_sentence(s_pass, r_pass))
-    word_list = add_japanese(sort_count(word_dict))
-    output(list_to_json(word_list))
-    print("\n英単語帳が作成されました\n")
-    print_word_count_mean()
+    if check_pass(s_pass):
+        word_dict = count_per_word(cut_sentence(s_pass, r_pass))
+        word_list = add_means(sort_format(word_dict))
+        output(list_to_json(word_list))
+        print("\n英単語帳が作成されました\n")
+        print_word_count_mean()
+    else:
+        print("指定されたファイルの形式が.txtではありません")
 
 # 作成された英単語帳から単語の意味を更新する
 def update_wordbook():
     try:
-        with open('output\\output.json', encoding='utf-8') as f:
+        with open('output\\output.json', "r", encoding='utf-8') as f:
             data_dict = json.load(f)
             print_word_mean()
             print("\nどの英単語の日本語訳を更新しますか？")
@@ -63,24 +69,49 @@ def update_wordbook():
                     data_dict[key]["means"] = input()
                     output(data_dict)
                     print_word_mean()
-                    print("更新しました")
+                    print("\n更新しました")
                     break
             else:
                 print("入力された英単語は存在しません\n")
     except FileNotFoundError:
-        print("はじめに英単語帳に作成を行ってください")
+        print("はじめに英単語帳の作成を行ってください")
+
+# JSON形式のファイルに格納されたデータを指定の方法でソートする
+def sort():
+    menu_list = [
+        "出現回数順　　　　: 1", 
+        "アルファベット順　: 2",
+        "終了　　　　　　　: 0"
+    ]
+    for s in menu_list:
+        print(s)
+    print("ソートしたい方法の番号を半角で入力してください")
+    select = input()
+    if select == "1":
+        sort_count()
+        print_word_count_mean()
+        print("\n出現回数順でソートされました")
+    elif select == "2":
+        sort_atoz()
+        print_word_count_mean()
+        print("\nアルファベット順でソートされました")
+    elif select == "0":
+        print("終了します")
+    else:
+        print("メニューに存在する番号を半角で入力してください")
+        sort()
 
 # 元の文を小文字に直し単語ごとのリストを作る。
 # その後そのリストから除外対象の要素を除外し返す。
 def cut_sentence(s_pass, r_pass):
-    with open(s_pass, "r") as f:
+    with open(s_pass, "r", encoding='utf-8') as f:
         sentence = f.read()
         word_list = re.findall('(\w+)[\W+]', sentence.lower())
     return remove(word_list, r_pass)
 
 # リストから数字のみの要素と除外リストの要素を除外する
 def remove(word_list, r_pass):
-    with open(r_pass, "r") as f:
+    with open(r_pass, "r", encoding='utf-8') as f:
         remove_list = f.read()
         i = 0
     while i < len(word_list):
@@ -102,48 +133,73 @@ def count_per_word(word_list):
 
 # 英単語と回数を表示する(仮、空白の数を考える(表示の形成))
 def print_word_count(word_list):
-    print("{:10}".format("word") + "{:10}".format("count"))
+    print("{:20}".format("word") + "{:10}".format("count"))
     for word, cnt in word_list:
-       print("{:10}".format(word) + "{:10}".format(str(cnt)))
+       print("{:20}".format(word) + "{:10}".format(str(cnt)))
 
 # JSON形式のファイルを読み込み英単語と意味を表示する(仮、空白の数を考える(表示の形成))
 def print_word_mean():
     try:
-        with open('output\\output.json', encoding='utf-8') as f:
-            print("{:10}".format("word") + "mean")
+        with open('output\\output.json', "r", encoding='utf-8') as f:
+            print("{:20}".format("word") + "mean")
             data_dict = json.load(f)
             for data in data_dict.values():
-                print("{:10}".format(data["name"]) + data["means"])
+                print("{:20}".format(data["name"]) + data["means"])
     except FileNotFoundError:
-        print("はじめに英単語帳に作成を行ってください")
+        print("はじめに英単語帳の作成を行ってください")
 
 # JSON形式のファイルを読み込み英単語と回数と意味を表示する(仮、空白の数を考える)
 def print_word_count_mean():
     try:
-        with open('output\\output.json', encoding='utf-8') as f:
-            print("{:10}".format("word") + "{:10}".format("count") + "mean")
+        with open('output\\output.json', "r", encoding='utf-8') as f:
+            print("{:20}".format("word") + "{:10}".format("count") + "mean")
             data_dict = json.load(f)
             for data in data_dict.values():
-                print("{:10}".format(data["name"]) + "{:<10}".format(data["count"]) + data["means"])
+                print("{:20}".format(data["name"]) + "{:<10}".format(data["count"]) + data["means"])
     except FileNotFoundError:
-        print("はじめに英単語帳に作成を行ってください")
+        print("はじめに英単語帳の作成を行ってください")
 
 # 英単語を出現回数でソートし上位10個だけをリストで返す
-def sort_count(word_dict):
+def sort_format(word_dict):
     word_list = []
     for k, v in sorted(word_dict.items(), key=lambda x: -x[1])[:10]:
         word_list.append([k, v])
     return word_list
 
-# 英単語をアルファベット順でソートし上位10個だけをリストで返す
-def sort_atoz(word_dict):
-    word_list = []
-    for k, v in sorted(word_dict.items(), key=lambda x: -x[0])[:10]:
-        word_list.append([k, v])
-    return word_list
+# 英単語を出現回数順でソートし出力しなおす
+def sort_count():
+    try:
+        with open('output\\output.json', "r", encoding='utf-8') as f:
+            data_dict = json.load(f)
+            sorted_dict = {}
+            cnt = 0
+            for _, v in sorted(data_dict.items(), key=lambda x: -x[1]["count"]):
+                sorted_dict["word" + str(cnt)] = v
+                cnt += 1
+            output(sorted_dict)
+    except FileNotFoundError:
+        print("はじめに英単語帳の作成を行ってください")
+
+# 英単語をアルファベット順でソートし出力しなおす
+def sort_atoz():
+    try:
+        with open('output\\output.json', "r", encoding='utf-8') as f:
+            data_dict = json.load(f)
+            sorted_dict = {}
+            cnt = 0
+            for _, v in sorted(data_dict.items(), key=lambda x: x[1]["name"]):
+                sorted_dict["word" + str(cnt)] = v
+                cnt += 1
+            output(sorted_dict)
+    except FileNotFoundError:
+        print("はじめに英単語帳の作成を行ってください")
+
+# 指定されたファイルのパスがテキストかどうかを判定する
+def check_pass(file_pass):
+    return re.findall('(\.txt$)', file_pass)
 
 # 英単語リストに和訳をつけたリストを返す
-def add_japanese(word_list):
+def add_means(word_list):
     print_word_count(word_list)
     new_word_list = []
     for word in word_list:
